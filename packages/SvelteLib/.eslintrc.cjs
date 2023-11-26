a27adc3d9566cdbe1fd308c1122b2d3d3efe542c
ArMar <mr.arthur.marchand@gmail.com>
2023-11-26 16:45:46 +0100

    u

--- packages/SvelteLib/.eslintrc.cjs
+++ packages/SvelteLib/.eslintrc.cjs
@@ -1,9 +1,24 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@repo/eslint-config/base.js'],
-  parser: '@typescript-eslint/parser',
+  extends: ["@repo/eslint-config/base.js"],
+
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
+    parser: "@typescript-eslint/parser",
+    project: "./tsconfig.json",
   },
-}
+  overrides: [
+    {
+      // From https://github.com/sveltejs/eslint-plugin-svelte
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+      rules: {
+        "@typescript-eslint/no-inferrable-types": "off",
+      },
+    },
+  ],
+};
